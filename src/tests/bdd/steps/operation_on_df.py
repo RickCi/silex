4c793e85c6d12e5517634ffdaa0dbc22b5e69f86
Aurélien Cluzeau <rickci@protonmail.com>
2022-08-26 15:09:18 +0200

    wip

--- src/tests/bdd/steps/operation_on_df.py
+++ src/tests/bdd/steps/operation_on_df.py
@@ -1,16 +1,5 @@
-import json
-
 from behave import step
 from pyspark.sql import DataFrame
-
-
-@step('we do "{op}" on table "{in_}" with parameters "{p}" and save to "{out}" table')
-def step_impl(context, op: str, in_: str, p: str, out: str):
-    params: dict = json.loads(p)
-    df: DataFrame = getattr(context, in_)
-    fn = getattr(df, op)
-    output: DataFrame = fn(**params)
-    setattr(context, out, output)
 
 
 @step('we do "{op}" on table "{in_}" and save to "{out}" table')
