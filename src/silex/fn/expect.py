29bfb02d46f44a3cec01dcaf45f76320cc6bb094
Aurélien Cluzeau <rickci@protonmail.com>
2022-09-10 19:01:07 +0200

    wip

--- src/silex/fn/expect.py
+++ src/silex/fn/expect.py
@@ -1,7 +1,45 @@
-from typing import List, Union
+from typing import Any, Collection, List, Union
 
 from pyspark.sql import DataFrame
 
 
 def expect_unique_id(df: DataFrame, cols: Union[str, List[str]]) -> DataFrame:
+    ...
+
+
+def expect_column(df: DataFrame, col: str) -> bool:
+    ...
+
+
+def expect_columns(df: DataFrame, cols: Union[str, List[str]]) -> bool:
+    ...
+
+
+def expect_distinct_values_in_set(
+    df: DataFrame, cols: Union[str, List[str]], values: Collection[Any]
+) -> bool:
+    ...
+
+
+def expect_distinct_values_equal_set(
+    df: DataFrame, cols: Union[str, List[str]], values: Collection[Any]
+) -> bool:
+    ...
+
+
+def expect_min_value_between(
+    df: DataFrame, cols: Union[str, List[str]], min_v: Any, max_v: Any
+) -> bool:
+    ...
+
+
+def expect_avg_value_between(
+    df: DataFrame, cols: Union[str, List[str]], min_v: Any, max_v: Any
+) -> bool:
+    ...
+
+
+def expect_max_value_between(
+    df: DataFrame, cols: Union[str, List[str]], min_v: Any, max_v: Any
+) -> bool:
     ...
