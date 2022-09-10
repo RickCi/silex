37112cf97ff95194ee3bfbe7332f284b0fb111ea
Aurélien Cluzeau <rickci@protonmail.com>
2022-09-10 18:55:39 +0200

    wip

--- src/silex/testing/string.py
+++ src/silex/testing/string.py
@@ -2,17 +2,35 @@ from contextlib import suppress
 from typing import Union
 
 
-def parse(s: str) -> Union[None, str, int, float]:
+def parse(s: str) -> Union[None, str, bool, int, float]:
     """Try to cast to:
         1. float
         2. int
 
     Returns: cast float, cast int or input string
+
+    >>> parse("<null>") is None
+    True
+    >>> parse("<true>")
+    True
+    >>> parse("<false>")
+    False
+    >>> parse("1")
+    1
+    >>> parse("1.0")
+    1.0
+    >>> parse("a")
+    'a'
     """
-    if s == "<NULL>":
+    s_upper = s.upper()
+    if s_upper == "<NULL>":
         return None
-    with suppress(ValueError):
-        return float(s)
+    if s_upper == "<TRUE>":
+        return True
+    if s_upper == "<FALSE>":
+        return False
     with suppress(ValueError):
         return int(s)
+    with suppress(ValueError):
+        return float(s)
     return s
